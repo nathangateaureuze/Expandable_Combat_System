fc4f2c71e69778d7829822011f0399120d3cb94e
nathangateaureuze <92450570+nathangateaureuze@users.noreply.github.com>
2025-11-05 13:43:21 +0100

    copyrights

--- Source/CombatSystem/Fighter/FighterAction.h
+++ Source/CombatSystem/Fighter/FighterAction.h
@@ -1,4 +1,4 @@
-// Fill out your copyright notice in the Description page of Project Settings.
+// Permission to use, copy, modify, and/or distribute this software for any purpose with or without fee is hereby granted.
 
 #pragma once
 
